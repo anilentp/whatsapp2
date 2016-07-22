615374c026dd97350ba5ccaf7507f0d0f63f8764
AniiLen <anile.tp@gmail.com>
2016-07-22 08:59:01 -0400

    details

--- js/main.js
+++ js/main.js
@@ -1,30 +1,3 @@
-// search
- 
- $('ul.listado li a').click(function() {
-    var textoFiltro = $(this).text().toLowerCase().replace(/\s/g,"-");
-    if(textoFiltro == 'todos')
-    {
-        $('div.filtros div.hidden').fadeIn('slow').removeClass('hidden');
-    }
-    else
-    {
-        $('.filtros div').each(function()
-        {
-            if(!$(this).hasClass(filterVal))
-            {
-                $(this).fadeOut('normal').addClass('hidden');
-            }
-            else
-            {
-                $(this).fadeIn('slow').removeClass('hidden');
-            }
-        });
-    }
-    return false;
-    });
-}
- 
-// search
 
 function valorInput() {
     var inputBox = document.getElementById("tarea");
